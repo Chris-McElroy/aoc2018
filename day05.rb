8947a8e303cb21103808a6d3c86d688989c1eea8
ChristopherMcElroy <csmcelroy@me.com>
2021-08-31 15:17:07 -0700

    10*

--- day05.rb
+++ day05.rb
@@ -1,3 +1,21 @@
 # frozen_string_literal: true
 
 require './helpers'
+
+def input1
+  1
+end
+
+def input2
+  5
+end
+
+def print_output(output)
+  p output if output != 0
+end
+
+run_intcode(intcode(5), :input1, :print_output)
+run_intcode(intcode(5), :input2, :print_output)
+
+# 16434972
+# 16694270
--- helpers.rb
+++ helpers.rb
@@ -99,27 +99,68 @@ def dir_from_nsew_hash
   { 'N' => C2.new(0, 1), 'S' => C2.new(0, -1), 'E' => C2.new(1, 0), 'W' => C2.new(-1, 0) }
 end
 
-def run_intcode(code)
+def run_intcode(code, pull_input, push_output)
   line = 0
   while code[line] != 99
-    case code[line]
+    op = code[line] % 100
+    case op
     when 1..2
       code[code[line + 3]] = arithmetic_op(code, line)
-      line += 4
+    when 3
+      code[code[line + 1]] = method(pull_input).call
+    when 4
+      method(push_output).call(code[line] > 100 ? code[line + 1] : code[code[line + 1]])
+    when 5..6
+      line = jump_op(code, line)
+    when 7..8
+      code[code[line + 3]] = equality_op(code, line)
     end
+    line += [0, 4, 4, 2, 2, 0, 0, 4, 4][op]
   end
   code[0]
 end
 
 def arithmetic_op(code, line)
-  param1 = code[line + 1] < code.size ? code[code[line + 1]] : 0
-  param2 = code[line + 2] < code.size ? code[code[line + 2]] : 0
-  case code[line]
+  param1, param2 = get_params(code, line)
+  case code[line] % 100
   when 1
     param1 + param2
   when 2
     param1 * param2
   end
+end
+
+def jump_op(code, line)
+  param1, param2 = get_params(code, line)
+  if (code[line] % 100 == 6) == (param1.zero?)
+    param2
+  else
+    line + 3
+  end
+end
+
+def equality_op(code, line)
+  param1, param2 = get_params(code, line)
+  case code[line] % 100
+  when 7
+    param1 < param2 ? 1 : 0
+  when 8
+    param1 == param2 ? 1 : 0
+  end
+end
+
+def get_params(code, line)
+  param1 = code[line + 1]
+  param2 = code[line + 2]
+  if code[line] > 1000
+    code[line] -= 1000
+  else
+    param2 = param2 < code.size ? code[param2] : 0
+  end
+
+  param1 = param1 < code.size ? code[param1] : 0 if code[line] < 100
+
+  [param1, param2]
 end
 
 def min(a, b)
